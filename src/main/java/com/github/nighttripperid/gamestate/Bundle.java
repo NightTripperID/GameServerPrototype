f2a2df79807bf88601111445836355606ace2a5d
NightTripperId <ndering@wgu.edu>
2021-02-19 12:33:24 -0700

    integrated TinySound

--- src/main/java/com/github/nighttripperid/gamestate/Bundle.java
+++
@@ -1,8 +0,0 @@
-package com.github.nighttripperid.gamestate;
-
-/**
- * Object representing a bundle of primitive and complex objexts. Values are stored in lazily instantiated HashMaps.
- *
- */
-public final class Bundle extends HashMaps {
-}
--- src/main/java/com/github/nighttripperid/gamestate/Updatable.java
+++
@@ -1,9 +0,0 @@
-package com.github.nighttripperid.gamestate;
-
-/**
- * Classes that exercise game logic must implement this interface. Game logic will be contained inside this method.
- */
-
-public interface Updatable {
-    void update();
-}
