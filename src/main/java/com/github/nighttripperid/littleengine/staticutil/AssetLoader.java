7046f384b78d76dfaeba9bb1afc7d0f976f20c9d
NightTripperId <ndering@wgu.edu>
2021-03-05 14:51:41 -0700

    merged spriteKey into filePath argument to simplify entity sprite asset loading

--- src/main/java/com/github/nighttripperid/littleengine/staticutil/AssetLoader.java
+++ src/main/java/com/github/nighttripperid/littleengine/staticutil/AssetLoader.java
@@ -29,6 +29,9 @@ package com.github.nighttripperid.littleengine.staticutil;
 import com.github.cliftonlabs.json_simple.JsonException;
 import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
+import com.github.nighttripperid.littleengine.model.PointDouble;
+import com.github.nighttripperid.littleengine.model.entity.Entity;
+import com.github.nighttripperid.littleengine.model.gamestate.EntityData;
 import com.github.nighttripperid.littleengine.model.gamestate.GameMap;
 import com.github.nighttripperid.littleengine.model.tiles.TileMap;
 import com.github.nighttripperid.littleengine.model.graphics.SpriteSheet;
@@ -44,29 +47,60 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
+// TODO: formulate a way to decouple entity loading from tilemap loading for sake of clarity
 @Slf4j
-public class MapLoader {
-    private MapLoader() {
+public class AssetLoader {
+    private AssetLoader() {
     }
 
-    public static void loadTileMap(String pngPath, String jsonPath, GameMap gameMap) {
+    public static void loadAssets(String pngPath, String jsonPath, GameMap gameMap, EntityData entityData) {
+        loadTileMap(jsonPath, gameMap, entityData);
+        loadTileset(pngPath, gameMap);
+    }
 
-        URL url = MapLoader.class.getClassLoader().getResource(jsonPath);
+    private static void loadTileMap(String jsonPath, GameMap gameMap, EntityData entityData) {
+        URL url = AssetLoader.class.getClassLoader().getResource(jsonPath);
+        assert url != null;
         log.info("Loading: {}{}", url.getPath(), "...");
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
             JsonObject jsonObject = (JsonObject) Jsoner.deserialize(reader);
             Mapper mapper = new DozerBeanMapper();
             TILED_TileMap tiled_TileMap = mapper.map(jsonObject, TILED_TileMap.class);
 
-            gameMap.setTileMap(new TileMap());
-            gameMap.getTileMap().setTiled_TileMap(tiled_TileMap);
             gameMap.setTileSize(tiled_TileMap.getTilewidth());
             gameMap.setTileBitShift((int) (Math.log(gameMap.getTileSize()) / Math.log(2)));
+            gameMap.setTileMap(new TileMap());
+            gameMap.getTileMap().setTiled_TileMap(tiled_TileMap);
 
             tiled_TileMap.getLayers().forEach(layer -> {
                 if (layer.getType().equals("tilelayer"))
-                gameMap.getTileMap().putLayer(layer.getId(), layer.getData());
+                    gameMap.getTileMap().putLayer(layer.getId(), layer.getData());
+                if (layer.getType().equals("objectgroup")) {
+                    layer.getObjects().forEach(object -> {
+                        if (object.getType().equals("entity")) {
+                            Map<String, String> properties = new HashMap<>();
+                            object.getProperties().forEach(property -> {
+                                properties.put(property.getName(), property.getValue());
+                            });
+                            try {
+                                Class<?> clazz = Class.forName(properties.get("class"));
+                                Entity entity = (Entity) clazz.newInstance();
+                                entity.setPosition(new PointDouble(object.getX(), object.getY()));
+                                entity.setSpriteKey(properties.get("filename"));
+                                entityData.getEntities().add(entity);
+                            } catch (ClassNotFoundException e) {
+                                log.error("ClassNotFoundException thrown");
+                            } catch (IllegalAccessException e) {
+                                log.error("IllegalAccessException thrown");
+                            } catch (InstantiationException e) {
+                                log.error("InstantiationException thrown");
+                            }
+                        }
+                    });
+                }
             });
 
             log.info("Loading {} successful!", url.getPath());
@@ -74,14 +108,12 @@ public class MapLoader {
         } catch (IOException | JsonException e) {
             log.error("Loading {} failed!", url.getPath());
         }
-
-        loadTileset(pngPath, gameMap);
     }
 
     private static void loadTileset(String pngPath, GameMap gameMap) {
-
-        URL url = MapLoader.class.getClassLoader().getResource(pngPath);
+        URL url = AssetLoader.class.getClassLoader().getResource(pngPath);
         try {
+            assert url != null;
             log.info("Loading: {}{}", url.getPath(), "...");
             BufferedImage image = ImageIO.read(url);
             log.info("Loading {} successful!", url.getPath());
